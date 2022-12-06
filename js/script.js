b425d345f530052c15a77e9d4052186b61420c67
Bartosz Dabrowski <bartosz.s.dabrowski@gmail.com>
2022-12-06 23:27:04 +0100

    Change functions into anonymous functions

--- js/script.js
+++ js/script.js
@@ -3,7 +3,20 @@
 
         function clearMessages(){}
 
-        function getMoveName(argMoveId){
+        /* function getMoveName(argMoveId){
+            if(argMoveId == 1){
+                return 'kamień';
+            } else if (argMoveId == 2){
+                return 'papier';
+            } else if (argMoveId == 3){
+                return 'nożyce';
+            } else {
+                printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+                return 'nieznany ruch';
+            }
+        } */
+
+        const getMoveName = function(argMoveId){
             if(argMoveId == 1){
                 return 'kamień';
             } else if (argMoveId == 2){
@@ -16,7 +29,7 @@
             }
         }
 
-        function displayResult(argComputerMove, argPlayerMove){
+        const displayResult = function(argComputerMove, argPlayerMove){
             console.log('Komputer wybiera ' + argComputerMove + ', gracz wybiera ' + argPlayerMove);
             printMessage('Ja wybieram ' + argComputerMove + ', Ty wybierasz ' + argPlayerMove + '.');
 
