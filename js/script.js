52f1a9de27e292e2c691f235cc03ee4aef9c4bda
Bartosz Dabrowski <bartosz.s.dabrowski@gmail.com>
2022-12-07 00:18:50 +0100

    Change function to make it shorter

--- js/script.js
+++ js/script.js
@@ -20,22 +20,12 @@
             console.log('Komputer wybiera ' + argComputerMove + ', gracz wybiera ' + argPlayerMove);
             printMessage('Ja wybieram ' + argComputerMove + ', Ty wybierasz ' + argPlayerMove + '.');
 
-            if(argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+            if((argComputerMove == 'kamień' && argPlayerMove == 'papier') || (argComputerMove == 'papier' && argPlayerMove == 'nożyce') || (argComputerMove == 'nożyce' && argPlayerMove == 'kamień')){
                 printMessage('Ty wygrywasz!');
-            } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
-                printMessage('Ty wygrywasz!');
-            } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
-                printMessage('Ty wygrywasz!');
-            } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
+            } else if ((argComputerMove == 'kamień' && argPlayerMove == 'nożyce') || (argComputerMove == 'papier' && argPlayerMove == 'kamień') || (argComputerMove == 'nożyce' && argPlayerMove == 'papier')) {
                 printMessage('Ja wygrywam!');
-            } else if (argComputerMove == 'papier' && argPlayerMove == 'kamień'){
-                printMessage('Ja wygrywam!');
-            } else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
-                printMessage('Ja wygrywam!');
-            } else if (argComputerMove == argPlayerMove){
-                printMessage('Remis!');
             } else {
-                printMessage('Gra nierozsrzygnięta');
+                printMessage('Remis!');
             }
         }
 
